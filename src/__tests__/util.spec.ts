ae265cd201d31b385372e9d6a1edb90285ff6919
Kevin von Flotow <vonflow@gmail.com>
2023-05-14 04:45:11 -0600

    add urlName utils and tests

--- src/__tests__/util.spec.ts
+++ src/__tests__/util.spec.ts
@@ -1,4 +1,8 @@
-import { getNetworkDataByChainId } from '../util'
+import {
+  getChainIdByUrlName,
+  getNetworkDataByChainId,
+  getNetworkDataByUrlName,
+} from '../util'
 
 describe('getNetworkDataByChainId()', () => {
   it('returns the requested network', () => {
@@ -9,5 +13,29 @@ describe('getNetworkDataByChainId()', () => {
   it('returns undefined with invalid network', () => {
     const network = getNetworkDataByChainId(-42378473298432)
     expect(network).toBeUndefined()
+  })
+})
+
+describe('getNetworkDataByUrlName()', () => {
+  it('returns the requested network', () => {
+    const { chainId } = getNetworkDataByUrlName('eth')
+    expect(chainId).toStrictEqual(1)
+  })
+
+  it('returns undefined with invalid network', () => {
+    const network = getNetworkDataByUrlName('definitely-invalid')
+    expect(network).toBeUndefined()
+  })
+})
+
+describe('getChainIdByUrlName()', () => {
+  it('returns the correct chain id', () => {
+    const chainId = getChainIdByUrlName('eth')
+    expect(chainId).toStrictEqual(1)
+  })
+
+  it('returns undefined with invalid network url name', () => {
+    const chainId = getChainIdByUrlName('definitely-invalid')
+    expect(chainId).toBeUndefined()
   })
 })
--- src/util.ts
+++ src/util.ts
@@ -13,3 +13,27 @@ export const getNetworkDataByChainId = (
 ): NetworkData | undefined => {
   return networksArray.find(({ chainId }) => chainId === targetChainId)
 }
+
+/**
+ *
+ * @param targetUrlName url name of the network to look for
+ * @return {@link NetworkData} matching `targetUrlName` or `undefined` if not found
+ */
+export const getNetworkDataByUrlName = (
+  targetUrlName: string,
+): NetworkData | undefined => {
+  return networksArray.find(({ urlName }) => urlName === targetUrlName)
+}
+
+/**
+ * Helper function to convert network url name into chain id
+ *
+ * @param targetUrlName url name of the network to look for
+ * @return `chainId` for the network with matching `urlName` or `undefined` if not found
+ */
+export const getChainIdByUrlName = (
+  targetUrlName: string,
+): number | undefined => {
+  const network = getNetworkDataByUrlName(targetUrlName)
+  return network?.chainId
+}
