6447b8f34a43edf2c49e826eecf088e64c97065d
Kevin von Flotow <vonflow@gmail.com>
2023-05-14 14:07:33 -0600

    add fuse

---
+++ src/networks/122-fuse.ts
@@ -0,0 +1,27 @@
+import { NetworkData } from '../types'
+
+const network: NetworkData = {
+  chainId: 122,
+  name: 'Fuse',
+  shortName: 'Fuse',
+  urlName: 'fuse',
+  nativeCurrency: {
+    address: '0x0BE9e53fd7EDaC9F859882AfdDa116645287C629',
+    name: 'Fuse',
+    symbol: 'FUSE',
+    decimals: 18,
+  },
+  explorerURL: 'https://explorer.fuse.io/',
+  rpcURL: 'https://rpc.fuse.io/',
+  isTestNet: false,
+  liquidityPairsUniV2: [
+    {
+      // WFUSE
+      tokenAddress: '0x0BE9e53fd7EDaC9F859882AfdDa116645287C629',
+      // WFUSE / USDT lp on app.fuse.fi
+      stablePairAddress: '0x8a81984D2DF356B49d182910bbB935897450d7e8',
+    },
+  ],
+}
+
+export default network
