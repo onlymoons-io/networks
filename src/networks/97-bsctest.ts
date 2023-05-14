b317dad2f7ea891e96ca04db693596b1aa97af0d
Kevin von Flotow <vonflow@gmail.com>
2023-05-14 15:30:42 -0600

    update liquidity pair types

--- src/networks/97-bsctest.ts
+++ src/networks/97-bsctest.ts
@@ -14,13 +14,10 @@ const network: NetworkData = {
   explorerURL: 'https://testnet.bscscan.com/',
   rpcURL: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
   isTestNet: true,
-  liquidityPairsUniV2: [
-    {
-      tokenAddress: '0xae13d989dac2f0debff460ac112a837c89baa7cd',
-      // pair w/ USDT on https://pancake.kiemtienonline360.com/
-      stablePairAddress: '0xf855e52ecc8b3b795ac289f85f6fd7a99883492b',
-    },
-  ],
+  liquidityPairUniV2: {
+    // pair w/ USDT on https://pancake.kiemtienonline360.com/
+    stablePairAddress: '0xf855e52ecc8b3b795ac289f85f6fd7a99883492b',
+  },
 }
 
 export default network
