189f4a2e3594ccee7c6aefc026577e12f6a8ea1b
Kevin von Flotow <vonflow@gmail.com>
2023-05-14 15:37:21 -0600

    use new format for liquidity pair

--- src/networks/137-polygon.ts
+++ src/networks/137-polygon.ts
@@ -13,14 +13,10 @@ const network: NetworkData = {
   },
   explorerURL: 'https://polygonscan.com/',
   rpcURL: 'https://polygon-rpc.com/',
-  liquidityPairsUniV2: [
-    // WMATIC
-    {
-      tokenAddress: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
-      // pair w/ USDC on quickswap
-      stablePairAddress: '0x6e7a5FAFcec6BB1e78bAE2A1F0B612012BF14827',
-    },
-  ],
+  liquidityPairUniV2: {
+    // pair w/ USDC on quickswap
+    stablePairAddress: '0x6e7a5FAFcec6BB1e78bAE2A1F0B612012BF14827',
+  },
 }
 
 export default network
