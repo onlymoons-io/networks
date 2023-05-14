fc5d93a411637826cc539b950955d989146d7fca
Kevin von Flotow <vonflow@gmail.com>
2023-05-14 04:25:19 -0600

    add bsc testnet

---
+++ src/networks/97-bsctest.ts
@@ -0,0 +1,24 @@
+import { NetworkData } from '../types'
+
+export default <NetworkData>{
+  chainId: 97,
+  name: 'Binance Smart Chain (testnet)',
+  shortName: 'BSC (testnet)',
+  urlName: 'bsctest',
+  nativeCurrency: {
+    address: '0xae13d989dac2f0debff460ac112a837c89baa7cd',
+    name: 'Binance Coin',
+    symbol: 'BNB',
+    decimals: 18,
+  },
+  explorerURL: 'https://testnet.bscscan.com/',
+  rpcURL: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+  isTestNet: true,
+  liquidityPairsUniV2: [
+    {
+      tokenAddress: '0xae13d989dac2f0debff460ac112a837c89baa7cd',
+      // pair w/ USDT on https://pancake.kiemtienonline360.com/
+      stablePairAddress: '0xf855e52ecc8b3b795ac289f85f6fd7a99883492b',
+    },
+  ],
+}
