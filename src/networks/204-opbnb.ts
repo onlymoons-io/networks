dfec25a3c5e8a8c516f6b5957ce9a7f181308998
Deciphersoul <Deciphersoul@gmail.com>
2024-01-08 16:44:21 -0600

    Op bnb (#31)

---
+++ src/networks/204-opbnb.ts
@@ -0,0 +1,18 @@
+import { NetworkData } from '../types'
+
+const network: NetworkData = {
+  chainId: 204,
+  name: 'opBNB',
+  shortName: 'opBNB',
+  urlName: 'opbnb',
+  nativeCurrency: {
+    address: '0x4200000000000000000000000000000000000006',
+    name: 'Binance',
+    symbol: 'BNB',
+    decimals: 18,
+  },
+  explorerURL: 'https://mainnet.opbnbscan.com/',
+  rpcURL: 'https://opbnb-mainnet-rpc.bnbchain.org/',
+}
+
+export default network
