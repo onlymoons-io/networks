4a7b7159b7754f76647d2492cdcad8a10ddd37e5
Deciphersoul <Deciphersoul@gmail.com>
2024-01-08 16:50:00 -0600

    scroll (#33)

---
+++ src/networks/534352-scroll.ts
@@ -0,0 +1,18 @@
+import { NetworkData } from '../types'
+
+const network: NetworkData = {
+  chainId: 534352,
+  name: 'Scroll',
+  shortName: 'Scroll',
+  urlName: 'scroll',
+  nativeCurrency: {
+    address: '0x5300000000000000000000000000000000000004',
+    name: 'Ethereum',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  explorerURL: 'https://blockscout.scroll.io',
+  rpcURL: 'https://rpc.scroll.io',
+}
+
+export default network
