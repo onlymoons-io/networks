d26f50b8345d400b827c49fc32dab249bf625e34
Kevin <vonflow@gmail.com>
2024-03-05 02:51:32 -0700

    Blast (#43)
    * blast
    
    * formatting, update package lock

---
+++ src/networks/81457-blast.ts
@@ -0,0 +1,18 @@
+import { NetworkData } from '../types'
+
+const network: NetworkData = {
+  chainId: 81457,
+  name: 'Blast',
+  shortName: 'Blast',
+  urlName: 'blast',
+  nativeCurrency: {
+    address: '0x4300000000000000000000000000000000000004',
+    name: 'Ethereum',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  explorerURL: 'https://blastscan.io/',
+  rpcURL: 'https://blast.blockpi.network/v1/rpc/public',
+}
+
+export default network
