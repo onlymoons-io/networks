c056d45eaa6f88d490e0fc07199ddf8890575166
Kevin <vonflow@gmail.com>
2024-03-05 03:25:42 -0700

    V0.0.16 (#44)
    * Blast (#43)
    
    * blast
    
    * formatting, update package lock
    
    * update package

--- src/networks/4200-merlin.ts
+++ src/networks/4200-merlin.ts
@@ -1,18 +1,18 @@
 import { NetworkData } from '../types'
 
 const network: NetworkData = {
-	chainId: 4200,
-	name: 'Merlin',
-	shortName: 'Merlin',
-	urlName: 'merlin',
-	nativeCurrency: {
-		address: '0xF6D226f9Dc15d9bB51182815b320D3fBE324e1bA',
-		name: 'Bitcoin',
-		symbol: 'BTC',
-		decimals: 18,
-	},
-	explorerURL: 'https://scan.merlinchain.io/',
-	rpcURL: 'https://rpc.merlinchain.io',
+  chainId: 4200,
+  name: 'Merlin',
+  shortName: 'Merlin',
+  urlName: 'merlin',
+  nativeCurrency: {
+    address: '0xF6D226f9Dc15d9bB51182815b320D3fBE324e1bA',
+    name: 'Bitcoin',
+    symbol: 'BTC',
+    decimals: 18,
+  },
+  explorerURL: 'https://scan.merlinchain.io/',
+  rpcURL: 'https://rpc.merlinchain.io',
 }
 
 export default network
