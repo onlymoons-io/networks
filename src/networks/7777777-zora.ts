efda5720ba636198fb54e3313f9db977dcebdf2c
Deciphersoul <Deciphersoul@gmail.com>
2024-03-07 18:36:29 -0600

    zora

---
+++ src/networks/7777777-zora.ts
@@ -0,0 +1,18 @@
+import { NetworkData } from '../types'
+
+const network: NetworkData = {
+	chainId: 7777777,
+	name: 'Zora',
+	shortName: 'Zora',
+	urlName: 'zora',
+	nativeCurrency: {
+		address: '0',
+		name: 'Ethereum',
+		symbol: 'ETH',
+		decimals: 18,
+	},
+	explorerURL: 'https://explorer.zora.energy/',
+	rpcURL: 'https://rpc.zora.energy',
+}
+
+export default network
